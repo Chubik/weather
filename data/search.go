98c1c357344409378f19847dca0ab53d043aefed
Valentyn Chub <valentin.chub@gmail.com>
2018-10-23 20:52:21 +0300

    bug fix with json to string

--- data/search.go
+++ data/search.go
@@ -1,24 +1,22 @@
 package data
 
 import (
-	"encoding/json"
-	"log"
 	"strings"
 )
 
 //Search method for searches in string
-func Search(what string) []string {
-	var resp []string
+func Search(what string) []*CityItem {
+	var resp []*CityItem
 
 	for _, city := range Cities {
 		if strings.Contains(strings.ToLower(city.Name), what) {
-			ret, err := json.Marshal(city)
-			if err != nil {
-				log.Println("Error parsing city data", err)
-			}
-			resp = append(resp, string(ret))
+			// ret, err := json.Marshal(city)
+			// if err != nil {
+			// 	log.Println("Error parsing city data", err)
+			// }
+
+			resp = append(resp, city)
 		}
 	}
-
 	return resp
 }
